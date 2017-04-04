1090476be311a02b17309684dc028fb5b2733634
vinodkottem <vinodkottem@gmail.com>
2017-04-04 11:04:20 +0530

    initial

---
+++ test/index.js
@@ -0,0 +1,10 @@
+var summarize = require('summarize');
+var superagent = require('superagent');
+
+superagent
+  .get('http://stackoverflow.com/questions/38838281/piping-readable-stream-using-superagent')
+  .end(function(err, res){
+	  //console.log(err);
+	  console.log(res);
+    console.log(summarize(res.text,10));
+  });
