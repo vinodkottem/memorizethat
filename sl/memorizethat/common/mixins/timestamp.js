78a212defeee9e282a6cfdb272ec0a016e67fbba
vinodkottem <vinodkottem@gmail.com>
2017-04-29 22:22:00 +0530

    okay

--- sl/memorizethat/common/mixins/timestamp.js
+++ sl/memorizethat/common/mixins/timestamp.js
@@ -3,4 +3,5 @@ module.exports = function(Model, options) {
   // options is an object containing the config properties from model definition
   Model.defineProperty('created', {type: Date, default: '$now'});
   Model.defineProperty('modified', {type: Date, default: '$now'});
+  Model.defineProperty('myOption', {type: "string", default: 'amgone'});
 }
